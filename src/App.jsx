48f3976cfb0c0092890ac2d1752c4a137ead8bf4
DeadlySage <adrian.hartanto159@gmail.com>
2025-04-01 10:13:21 -0700

    added dogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
+import { puppyList } from "./data";
 import "./App.css";
 
 function App() {
-  //const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  const [feetPupId, setFeetPupId] = useState(null)
+  console.log("puppyList: ", puppyList);
+  
+  const featuredPup = puppies.find((pup)=> pup.id === feetPupId)
+  console.log(featuredPup)
   return (
     <>
-      <div></div>
+      <div>
+        {feetPupId && (
+          <div className="card">
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+        {puppies.map((puppies) => {
+          return <p onClick={()=>{setFeetPupId(puppies.id)}} key={puppies.id}>{puppies.name}</p>;
+        })}
+      </div>
     </>
   );
 }
